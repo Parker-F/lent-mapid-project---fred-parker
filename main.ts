769ea3e7da8b4df2400259f4aa83e2ad9e012b76
Parker-F <77438020+Parker-F@users.noreply.github.com>
2021-01-28 18:11:19 +0000

    Added multiple levels, enemies, health and everything

--- main.ts
+++ main.ts
@@ -1,9 +1,70 @@
+function StartNextLevel () {
+    currentlevel += 1
+    if (currentlevel == 1) {
+        tiles.setTilemap(tilemap`level0`)
+    } else if (currentlevel == 2) {
+        tiles.setTilemap(tilemap`level7`)
+    } else if (currentlevel == 3) {
+        tiles.setTilemap(tilemap`level8`)
+    } else if (currentlevel == 4) {
+    	
+    } else {
+        game.over(true, effects.confetti)
+    }
+    tiles.placeOnRandomTile(mySprite, sprites.dungeon.collectibleInsignia)
+    for (let value of tiles.getTilesByType(sprites.dungeon.collectibleRedCrystal)) {
+        myEnemy = sprites.create(img`
+            ........................
+            ........................
+            ........................
+            ........................
+            ..........ffff..........
+            ........ff1111ff........
+            .......fb111111bf.......
+            .......f11111111f.......
+            ......fd11111111df......
+            ......fd11111111df......
+            ......fddd1111dddf......
+            ......fbdbfddfbdbf......
+            ......fcdcf11fcdcf......
+            .......fb111111bf.......
+            ......fffcdb1bdffff.....
+            ....fc111cbfbfc111cf....
+            ....f1b1b1ffff1b1b1f....
+            ....fbfbffffffbfbfbf....
+            .........ffffff.........
+            ...........fff..........
+            ........................
+            ........................
+            ........................
+            ........................
+            `, SpriteKind.Enemy)
+        tiles.placeOnTile(myEnemy, value)
+        myEnemy.follow(mySprite, 30)
+    }
+}
+scene.onOverlapTile(SpriteKind.Player, assets.tile`tile`, function (sprite, location) {
+    game.over(false, effects.melt)
+})
 controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
     mySprite.vy = -120
 })
+scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.chestClosed, function (sprite, location) {
+    StartNextLevel()
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
+    otherSprite.destroy()
+    if (sprite.bottom < otherSprite.y) {
+        sprite.vy = -100
+    } else {
+        info.changeLifeBy(-1)
+    }
+})
+let myEnemy: Sprite = null
 let mySprite: Sprite = null
-scene.setBackgroundColor(6)
-tiles.setTilemap(tilemap`level2`)
+let currentlevel = 0
+currentlevel = 0
+scene.setBackgroundColor(11)
 mySprite = sprites.create(img`
     . . f f f . . . . . . . . f f f 
     . f f c c . . . . . . f c b b c 
@@ -22,7 +83,8 @@ mySprite = sprites.create(img`
     . . f b b b b b b c f . . . . . 
     . . . f f f f f f f . . . . . . 
     `, SpriteKind.Player)
-controller.moveSprite(mySprite, 100, 0)
 mySprite.ay = 500
+controller.moveSprite(mySprite, 100, 0)
 scene.cameraFollowSprite(mySprite)
-tiles.placeOnRandomTile(mySprite, sprites.dungeon.collectibleInsignia)
+info.setLife(3)
+StartNextLevel()
